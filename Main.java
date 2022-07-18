12b341135ee6017ddb7a9ad13a899a763e1254eb
KOWSHIK SAI RAJ <104454045+Kowshik-me@users.noreply.github.com>
2022-07-18 22:44:56 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+Main Java Program
+
+package javaapplication14;
+
+ 
+public class JavaApplication14 {
+ 
+    public static void main(String[] args) {
+            
+Calculator sc = new Calculator();
+
+    }
+    
+}
